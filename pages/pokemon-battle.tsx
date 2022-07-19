a8fc66b2e4c7bf5b30a7ea48dd674dcf06b641a5
Jassy567 <jassy.barkin567@gmail.com>
2022-07-19 16:16:48 +0900

    pagination overview

---
+++ pages/pokemon-battle.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+export default function PokemonBattle() {
+  return (
+    <div>
+      <Link href="/">
+        <p>トップに戻る</p>
+      </Link>
+      <p>ポケモンバトル</p>
+    </div>
+
+  )
+}
---
+++ pages/ranking.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+export default function Ranking() {
+  return (
+    <div>
+      <Link href="/">
+        <p>トップに戻る</p>
+      </Link>
+      <p>ランキング</p>
+    </div>
+
+  )
+}

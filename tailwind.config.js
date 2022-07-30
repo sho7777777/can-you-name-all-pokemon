78bf90880710318f3ec3dfd30b39713ccc16d8ce
Jassy567 <jassy.barkin567@gmail.com>
2022-07-30 10:53:15 +0900

    Revert "merge top-image-slide-in"
    This reverts commit 0b086a6c5b45c1a43800a6b9cf4abcf78d1c3e87, reversing
    changes made to 4e5bb454b3d9f70ae84e1e51330d7646e237d628.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ const Myclass = plugin(function ({ addUtilities }) {
     },
     ".backface-hidden": {
       backfaceVisibility: "hidden"
-    },
+    }
   })
 })
 
@@ -25,45 +25,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        slide: 'slide 20s infinite'
-      },
-      keyframes: {
-        slide: {
-          '0%': {
-            transform: "translateX(0)"
-          },
-          '20%': {
-            transform: "translateX(0)"
-          },
-          '25%': {
-            transform: "translateX(-100%)"
-          },
-          '40%': {
-            transform: "translateX(-100%)"
-          },
-          '45%': {
-            transform: "translateX(-200%)"
-          },
-          '60%': {
-            transform: "translateX(-200%)"
-          },
-          '65%': {
-            transform: "translateX(-300%)"
-          },
-          '80%': {
-            transform: "translateX(-300%)"
-          },
-          '85%': {
-            transform: "translateX(-400%)"
-          },
-          '100%': {
-            transform: "translateX(-400%)"
-          }
-        }
-      }
-    },
+    extend: {},
     screens: {
       sm: '480px',
       md: '768px',

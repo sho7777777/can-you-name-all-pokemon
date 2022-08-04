d9e4201e6274c002969e17fb6f50517e49526f9a
Jassy567 <jassy.barkin567@gmail.com>
2022-08-04 11:21:36 +0900

    Footer created

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,10 @@
 export const Footer = () => {
   return (
-    <div className="bg-cyan-200">
-      Footer
+    <div className="bg-cyan-200 text-xs text-gray-200">
+      <p>&copy; 2022 Jassy</p>
+      <p>本サイトはポケットモンスターの非公式二次創作です。株式会社ポケモンや任天堂、その他各企業様とは一切関係ありません。</p>
+      <p>ポケットモンスター・ポケモン・Pokémonは任天堂・クリーチャーズ・ゲームフリークの登録商標です。</p>
+
     </div>
   )
 }

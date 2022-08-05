e6b61a70db675efbfb1ef75a7a81189089ffcb56
Jassy567 <jassy.barkin567@gmail.com>
2022-08-05 23:31:28 +0900

    credit created

--- components/Top.tsx
+++ components/Top.tsx
@@ -75,6 +75,12 @@ export const Top = () => {
         </div>
       </div>
 
+      <div className="flex flex-col justify-center items-center mt-20 h-32 text-xs ">
+        <p className="text-gray-500">出典</p>
+        <p className="text-gray-500">ポケモン名（英語）の由来：<a href="https://pokemon-english-dictionary.com/" rel="noopener noreferrer" target="_blank" className='text-blue-500'>ポケモン英語語源辞典</a></p>
+        <p className="text-gray-500">英語音源：<a href="https://ondoku3.com/ja/" rel="noopener noreferrer" target="_blank" className='text-blue-500'>&copy;音読さん</a></p>
+      </div>
+
     </div>
   )
 }
